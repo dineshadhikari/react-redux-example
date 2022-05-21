70b109a708a17b314144d73309220df70c7cd764
Dinesh Adhikari <vipin.y@mernplus.com>
2022-05-21 17:30:54 +0530

    initial commit

---
+++ src/shop.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div>
+        Adidas shoe Rs. 50
+         <ul className="navbar-nav mr-auto">
+        <li className="nav-item btn">
+      <i className="bi bi-plus-square"></i>
+      </li>
+      <li className="nav-item btn">
+      <i className="bi bi-dash-square"></i>
+      </li>
+    </ul>
+    </div>
+  )
+}
